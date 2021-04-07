d65c41dd8211830631f7aa3f5b1047c047a2b04e
Tien Nguyen <monkiradgbn01@gmail.com>
2021-04-07 20:42:53 +0700

    update model.py

--- ultils/model.py
+++ ultils/model.py
@@ -13,3 +13,21 @@ class inception_V3(nn.Module):
         label1 = self.fc1(x)
         label2 = torch.sigmoid(self.fc2(x))
         return {'label1': label1, 'label2': label2}
+
+class Densenet(nn.Module):
+
+    def __init__(self):
+        super(Densenet, self).__init__()
+        self.model = models.densenet121(pretrained=True)
+        num_ftrs = self.model.classifier.in_features
+        self.fc = nn.Linear(num_ftrs, 100)  # For age class
+        self.fc1 = nn.Linear(num_ftrs, 2)  # For gender class
+
+    def forward(self, x):
+        label1 = self.fc1(x)
+        label2 = torch.sigmoid(self.fc2(x))
+
+        return {'label1': label1, 'label2': label2}
+
+if __name__ == '__main__':
+    model = Densenet()

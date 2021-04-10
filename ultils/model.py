95a0265529a5ab08f8c1f0f2631eac36b5788b84
Tien Nguyen <monkiradgbn01@gmail.com>
2021-04-10 15:24:02 +0700

    update

--- ultils/model.py
+++ ultils/model.py
@@ -1,6 +1,7 @@
 import torchvision.models as models
 from torch import nn
 import torch
+from torch.nn import functional as F
 
 class inception_V3(nn.Module):
     def __init__(self):
@@ -29,7 +30,23 @@ class Densenet(nn.Module):
 
         return {'label1': label1, 'label2': label2}
 
+
+class Resnet(nn.Module):
+    def __init__(self):
+        super(Resnet, self).__init__()
+        self.model = models.resnet34(pretrained="True")
+        self.fc1 = nn.Linear(512, 100)  # For age class
+        self.fc2 = nn.Linear(512, 2)  # For gender class
+
+    def forward(self, x):
+        bs, _, _, _ = x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(bs, -1)
+        label1 = self.fc1(x)
+        label2 = torch.sigmoid(self.fc2(x))
+        return {'label1': label1, 'label2': label2}
+
 if __name__ == '__main__':
     from torchsummary import summary
-    model = Densenet()
+    model = Resnet()
     summary(model, input_size=(3, 224, 224))

3ee9f624f2cf08e2f4b67b934af7ad92e4f8b74b
Tien Nguyen <monkiradgbn01@gmail.com>
2021-04-17 13:39:58 +0700

    update

--- ultils/loss.py
+++ ultils/loss.py
@@ -33,12 +33,19 @@ class Classify_Loss(nn.Module):
     def __init__(self, weight):
         super(Classify_Loss, self).__init__()
         self.weight = weight
+        self.log_vars = nn.Parameter(torch.ones((2)))
 
     def forward(self, age_hat, gender_hat, age, gender):
         # loss_age = nn.CrossEntropyLoss()
+        idx1 = (age < 20) | ((age > 40) & (age <= 60))
+        idx2 = age > 60
         loss_age = nn.NLLLoss()
         loss_gender = nn.NLLLoss()
-        loss1 = loss_age(age_hat, age)
-        loss2 = loss_age(gender_hat, gender)
-        loss_total = self.weight[0] * loss1 + self.weight[1] * loss2
+        loss1 = loss_age(age_hat, age) + 2 * loss_age(age_hat[idx1], age[idx1]) + 3 * loss_age(age_hat[idx2], age[
+            idx2])
+        loss1 = loss1*torch.exp(-self.log_vars[0])  + self.log_vars[0]
+        loss2 = loss_gender(gender_hat, gender)*torch.exp(-self.log_vars[1]) + self.log_vars[1]
+
+        #loss_total = self.weight[0] * loss1 + self.weight[1] * loss2
+        loss_total = loss1+loss2
         return loss_total

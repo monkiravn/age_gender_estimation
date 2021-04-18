d00d87950fba05012a1b48710ecf60c760f1a316
Tien Nguyen <monkiradgbn01@gmail.com>
2021-04-18 12:22:33 +0700

    update

--- training.sh
+++ training.sh
@@ -1,1 +1,1 @@
-python training.py --dftrain-path "data_processed/train.csv" --dftest-path "data_processed/val.csv" --dtroot-path "data_processed/crop" --mdsave-path "models" --fulltrain False --continues False
+python training.py --dftrain-path "data_processed/train.csv" --dftest-path "data_processed/val.csv" --dtroot-path "data_processed/crop" --mdsave-path "models" --train_mode "custom_both" --continues False
